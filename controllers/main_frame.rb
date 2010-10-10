2c0ce46f29b0b1b4f7caec0e1be5bb9e35c16721
Łukasz Korecki <lukasz@coffeesounds.com>
2010-10-10 20:49:45 +0100

    more better threading

--- controllers/main_frame.rb
+++ controllers/main_frame.rb
@@ -1,18 +1,21 @@
-class MainFrame < Wx::Frame
+class HMainFrame < MainFrame
   include Github
   def on_init
-    set_user_details
+    get_user_details { show_user_details }
   end
-  def set_user_details
-     Thread.new do
-      # FIXME this API sucks, the get_user_info needs to be hidden, no blocks here pls
-       @gh_user = User.new.info(:login => App.gh_login, :token => App.gh_token) do | auth_data |
-        Github.get_user_info auth_data
-      end
-      @user_name.label = "#{@gh_user["login"]} (#{@gh_user["name"]})"
-    end
 
+  def get_user_details
+    Thread.new do
+      @gh_user = User.new(:login => App.gh_login, :token => App.gh_token)
+      yield
+    end
   end
-  def gh_user_label
+
+  def show_user_details
+    @user_name.label = "lol: #{@gh_user.name} #{@gh_user.login}"
+
+    return 'stop here'
+    img = App.image_cache.do_it @gh_user.avatar, {:extension => "png", :subdir => 'avatars'}
+    @user_avatar.set_bitmap(Wx::Bitmap.from_image(img))
   end
 end
