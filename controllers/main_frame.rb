1b1dd5a3baa1b22088054b8835d0748783e00bdc
Łukasz Korecki <lukasz@coffeesounds.com>
2010-10-10 16:58:03 +0100

    suckkage uncovered\!

--- controllers/main_frame.rb
+++ controllers/main_frame.rb
@@ -1,5 +1,18 @@
 class MainFrame < Wx::Frame
+  include Github
   def on_init
-    @user_name.label = App.gh_login
+    set_user_details
+  end
+  def set_user_details
+     Thread.new do
+      # FIXME this API sucks, the get_user_info needs to be hidden, no blocks here pls
+       @gh_user = User.new.info(:login => App.gh_login, :token => App.gh_token) do | auth_data |
+        Github.get_user_info auth_data
+      end
+      @user_name.label = "#{@gh_user["login"]} (#{@gh_user["name"]})"
+    end
+
+  end
+  def gh_user_label
   end
 end
