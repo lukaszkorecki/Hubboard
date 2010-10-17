3e6394f43f9b1a2d125172714d10dac30d66b965
Łukasz Korecki <lukasz@coffeesounds.com>
2010-10-17 21:50:19 +0100

    [TODO] not tested, probably doesn't work

--- controllers/event_panel.rb
+++ controllers/event_panel.rb
@@ -1,6 +1,25 @@
 class HEventPanel < EventPanel
+  def on_init
+    evt_togglebutton @contents_toggle.get_id { | event | toggle_contents }
+  end
+
+  def toggle_contents
+    if @contents_toggle.value
+      @contents_html.page = @html unless @html.nil?
+      @contents_html.show
+    else
+      @contents_html.hide
+    end
+    @html = nil
+  end
   def body= event
-    @contents_html.page = "<h1>#{event[:title]}</h1>#{event[:content]}"
-    @user_name.label = event[:author][:name]
+    begin
+      @html = event[:contents]
+      @title_label.label = event[:title]
+      @user_link.label = event[:author][:name]
+      @user_link.url = event[:author][:uri]
+    rescue => e
+      STDOUT << e.to_yaml
+    end
   end
 end
