df5f67be0bde4f9a013c5694aaf089b47c0c87a5
Łukasz Korecki <lukasz@coffeesounds.com>
2010-10-20 22:37:02 +0100

    assign data to the panel elements

--- controllers/event_panel.rb
+++ controllers/event_panel.rb
@@ -1,25 +1,11 @@
 class HEventPanel < EventPanel
   def on_init
-    evt_togglebutton @contents_toggle.get_id { | event | toggle_contents }
   end
 
-  def toggle_contents
-    if @contents_toggle.value
-      @contents_html.page = @html unless @html.nil?
-      @contents_html.show
-    else
-      @contents_html.hide
-    end
-    @html = nil
-  end
   def body= event
-    begin
-      @html = event[:contents]
+      @contents_html.page = event[:content]
       @title_label.label = event[:title]
-      @user_link.label = event[:author][:name]
-      @user_link.url = event[:author][:uri]
-    rescue => e
-      STDOUT << e.to_yaml
-    end
+      @username_link.label = event[:author][:name]
+      @username_link.url = event[:author][:url]
   end
 end
