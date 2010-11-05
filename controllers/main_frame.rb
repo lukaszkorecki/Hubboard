001b810eebd2c9d8291346ff72f21678ad71cad3
Łukasz Korecki <lukasz@coffeesounds.com>
2010-11-05 23:36:15 +0000

    makes use of new email-client--like interface (makes event panel class obsolete)

--- controllers/main_frame.rb
+++ controllers/main_frame.rb
@@ -1,22 +1,33 @@
 class HMainFrame < MainFrame
   include Github
   def on_init
-    @timeline_scroller.sizer.layout
-    @timeline_scroller.set_scrollbars(5,5, 5,5,0,0,true)
 
     get_user_details { |details| show_user_details details }
     get_gh_dashboard { |entries|  show_dashboard entries }
+
+    evt_listbox(@title_list.get_id) { |ev| show_event_content ev }
+    evt_button(@visit_button.get_id) { Wx::launch_in_default_browser @current_url}
   end
 
+  def show_event_content ev
+    @event_content.page = format_content @entries[ev.index][:content]
+    @published_label.label = fuzzy_date @entries[ev.index][:published]
+    @title_label.label = @entries[ev.index][:title]
+    get_user_details @entries[ev.index][:author][:name] do |details|
+      show_user_details details
+    end
+    @current_url = @entries[ev.index][:link]
+  end
   def get_gh_dashboard
     Thread.new do
       entries = Feed.new(:login => App.gh_login, :token => App.gh_token).parse
       yield entries.empty? ? false : entries
     end
   end
-  def get_user_details
+  def get_user_details name = nil
+    user = name || {:login => App.gh_login, :token => App.gh_token}
     Thread.new do
-      gh_user = User.new(:login => App.gh_login, :token => App.gh_token)
+      gh_user = User.new(user)
       yield gh_user
     end
   end
@@ -29,19 +40,9 @@ class HMainFrame < MainFrame
   end
   def show_dashboard entries
     return missing_gh_cred unless entries
-
-    entries.each do |element|
-      begin
-        ev = HEventPanel.new @timeline_scroller
-        ev.body = element
-        @timeline_scroller.sizer.add ev, 1, Wx::GROW|Wx::ALL
-        @timeline_scroller.layout
-      rescue => e
-        STDOUT << e.to_yaml
-      end
-    end
-    @timeline_scroller.layout
-    @timeline_scroller.refresh
+    @entries = entries
+    ti = entries.map { |el| el[:title] }
+    @title_list.insert_items ti, 0
   end
   def message(title, text)
     m = Wx::MessageDialog.new(self, text, title, Wx::OK | Wx::ICON_INFORMATION)
@@ -52,5 +53,17 @@ class HMainFrame < MainFrame
   def missing_gh_cred
     message  APP_CONST['messages']['gitconfig_missing']['title'], APP_CONST['messages']['gitconfig_missing']['content'] unless @message_seen
     @message_seen = true
+  end
+private
+  def format_content content
+    # FIXME suboptimal
+
+    content.lines.to_a.reject{ | l | l =~ /div/ }.reject { | line | line.chomp.strip.empty? == true }.map do | line|
+      CGI::unescapeHTML line
+    end.join ""
+  end
+  def fuzzy_date(date_a)
+    date = DateTime.parse(date_a, true)
+    date.to_pretty
   end
 end
