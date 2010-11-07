1ea7101ad151d188abbb0820b9813ba2b593b7fe
Łukasz Korecki <lukasz@coffeesounds.com>
2010-11-07 13:53:40 +0000

    Removes event_panel - it's no longer needed

--- views/hubboard_views.rb
+++ views/hubboard_views.rb
@@ -4,7 +4,7 @@
 # this class and extend its behaviour there.  
 #
 # Source file: views/hubboard_views.xrc 
-# Generated at: Fri Nov 05 23:49:09 +0000 2010
+# Generated at: Sun Nov 07 13:53:12 +0000 2010
 
 class MainFrame < Wx::Frame
 	
@@ -62,53 +62,7 @@ end
 # this class and extend its behaviour there.  
 #
 # Source file: views/hubboard_views.xrc 
-# Generated at: Fri Nov 05 23:49:09 +0000 2010
-
-class EventPanel < Wx::Panel
-	
-	attr_reader :event_icon, :title_label, :ghlink_button, :user_avatar,
-              :contents_html, :published_label, :m_staticline1
-	
-	def initialize(parent = nil)
-		super()
-		xml = Wx::XmlResource.get
-		xml.flags = 2 # Wx::XRC_NO_SUBCLASSING
-		xml.init_all_handlers
-		xml.load("views/hubboard_views.xrc")
-		xml.load_panel_subclass(self, parent, "event_panel")
-
-		finder = lambda do | x | 
-			int_id = Wx::xrcid(x)
-			begin
-				Wx::Window.find_window_by_id(int_id, self) || int_id
-			# Temporary hack to work around regression in 1.9.2; remove
-			# begin/rescue clause in later versions
-			rescue RuntimeError
-				int_id
-			end
-		end
-		
-		@event_icon = finder.call("event_icon")
-		@title_label = finder.call("title_label")
-		@ghlink_button = finder.call("ghlink_button")
-		@user_avatar = finder.call("user_avatar")
-		@contents_html = finder.call("contents_html")
-		@published_label = finder.call("published_label")
-		@m_staticline1 = finder.call("m_staticline1")
-		if self.class.method_defined? "on_init"
-			self.on_init()
-		end
-	end
-end
-
-
-
-# This class was automatically generated from XRC source. It is not
-# recommended that this file is edited directly; instead, inherit from
-# this class and extend its behaviour there.  
-#
-# Source file: views/hubboard_views.xrc 
-# Generated at: Fri Nov 05 23:49:09 +0000 2010
+# Generated at: Sun Nov 07 13:53:12 +0000 2010
 
 class NewGistFrame < Wx::Frame
 	
