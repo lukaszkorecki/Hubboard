ae55801e3a169223612c4c63a7625b265c953f02
Łukasz Korecki <lukasz@coffeesounds.com>
2010-11-07 16:04:46 +0000

    preferences frame

--- views/hubboard_views.rb
+++ views/hubboard_views.rb
@@ -4,15 +4,15 @@
 # this class and extend its behaviour there.  
 #
 # Source file: views/hubboard_views.xrc 
-# Generated at: Sun Nov 07 13:53:12 +0000 2010
+# Generated at: Sun Nov 07 15:02:57 +0000 2010
 
 class MainFrame < Wx::Frame
 	
 	attr_reader :main_toolbar, :refresh_tool, :gist_new_tool,
-              :gist_browser_tool, :m_splitter1, :m_panel2,
-              :title_list, :m_panel3, :event_icon, :title_label,
-              :event_content, :published_label, :visit_button,
-              :details_html, :user_avatar, :status_bar
+              :gist_browser_tool, :preferences_tool, :m_splitter1,
+              :m_panel2, :title_list, :m_panel3, :event_icon,
+              :title_label, :event_content, :published_label,
+              :visit_button, :details_html, :user_avatar, :status_bar
 	
 	def initialize(parent = nil)
 		super()
@@ -37,6 +37,7 @@ class MainFrame < Wx::Frame
 		@refresh_tool = finder.call("refresh_tool")
 		@gist_new_tool = finder.call("gist_new_tool")
 		@gist_browser_tool = finder.call("gist_browser_tool")
+		@preferences_tool = finder.call("preferences_tool")
 		@m_splitter1 = finder.call("m_splitter1")
 		@m_panel2 = finder.call("m_panel2")
 		@title_list = finder.call("title_list")
@@ -62,7 +63,7 @@ end
 # this class and extend its behaviour there.  
 #
 # Source file: views/hubboard_views.xrc 
-# Generated at: Sun Nov 07 13:53:12 +0000 2010
+# Generated at: Sun Nov 07 15:02:57 +0000 2010
 
 class NewGistFrame < Wx::Frame
 	
@@ -97,6 +98,49 @@ class NewGistFrame < Wx::Frame
 		@m_textctrl21 = finder.call("m_textCtrl21")
 		@m_hyperlink1 = finder.call("m_hyperlink1")
 		@m_button1 = finder.call("m_button1")
+		if self.class.method_defined? "on_init"
+			self.on_init()
+		end
+	end
+end
+
+
+
+# This class was automatically generated from XRC source. It is not
+# recommended that this file is edited directly; instead, inherit from
+# this class and extend its behaviour there.  
+#
+# Source file: views/hubboard_views.xrc 
+# Generated at: Sun Nov 07 15:02:57 +0000 2010
+
+class PrefrencesFrame < Wx::Frame
+	
+	attr_reader :m_statictext7, :gh_username_text, :text2354,
+              :gh_token_text
+	
+	def initialize(parent = nil)
+		super()
+		xml = Wx::XmlResource.get
+		xml.flags = 2 # Wx::XRC_NO_SUBCLASSING
+		xml.init_all_handlers
+		xml.load("views/hubboard_views.xrc")
+		xml.load_frame_subclass(self, parent, "preferences_frame")
+
+		finder = lambda do | x | 
+			int_id = Wx::xrcid(x)
+			begin
+				Wx::Window.find_window_by_id(int_id, self) || int_id
+			# Temporary hack to work around regression in 1.9.2; remove
+			# begin/rescue clause in later versions
+			rescue RuntimeError
+				int_id
+			end
+		end
+		
+		@m_statictext7 = finder.call("m_staticText7")
+		@gh_username_text = finder.call("gh_username_text")
+		@text2354 = finder.call("text2354")
+		@gh_token_text = finder.call("gh_token_text")
 		if self.class.method_defined? "on_init"
 			self.on_init()
 		end
